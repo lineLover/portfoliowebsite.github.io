5d87c635a1d1b598c756b82bb69baf755290f01d
lineLover <124192403+lineLover@users.noreply.github.com>
2023-02-01 23:23:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* ============ Typing Animation ================== */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "UX/UI Designer", "Graphic Designer", "Web Developer", "Youtuber", "Vlogger", "C Programmer", "Python Programmer", "C++ Programmmer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
+/* ==================Aside =============== */
+
+/* 12_Oct_2022
+css(flexbox,grid,animation)
+js cwh(new oplaylist cntinue)
+figma bsictoadvanced
+
+*/
